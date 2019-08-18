7ec90502bba87d5d4ed693e3bbd9d4401b48871d
Julius Bacosa <juliusbacosa@gmail.com>
2019-08-19 07:47:10 +0800

    Rust

---
+++ ForLoop/apply.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let name = "Vlad";
+    println!("Hi! my name is {}!", name);
+
+    function_call();
+}
+
+fn function_call() {
+    println!("Yes!");
+}
---
+++ hello.rs
@@ -0,0 +1,6 @@
+fn main() {    
+    println!("Vlad Richie Bacosa!");
+    println!("{cool} {dad} {wife}", cool = "Vlad", dad = "Julz", wife = "Faith");
+
+    
+}
